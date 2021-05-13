4f11629d989a41634bf1dec548773fbd83063b74
Prakash <satya-prakash@sogeti.com>
2021-05-13 09:58:59 +0100

    Committing updated code

--- wdio.conf.js
+++ wdio.conf.js
@@ -12,7 +12,8 @@ if (!ENV || !['qa','dev','prod'].includes(ENV)) {
 exports.config = {
     //user: process.env.BROWSERSTACK_USERNAME,
     //key: process.env.BROWSERSTACK_ACCESS_KEY,
-,
+    //user: 'satyaprakash15',
+    //key: 'ts6f4CE53syybjLWbHMf',
     //port: 4723,
     //host: 'localhost',
     //path: '/wd/hub',
